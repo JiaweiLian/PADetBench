1fe629fb9126a4d47c3971d5166b987a92104969
Jianhong Pan <JianhongPan@outlook.com>
2024-05-31 01:51:31 +0800

    making the bounding box inside the image or being same to the bound of the image

--- export_datasets_with_coco_label.py
+++ export_datasets_with_coco_label.py
@@ -156,6 +156,8 @@ if __name__ == '__main__':
     default_dataset_len = 100
     settings = dict()
 
+    distance_range = (5,15)
+
     # benchmark settings
     if args.benchmark == 'vehicle':
         dataset_name='vehicle'
@@ -203,10 +205,10 @@ if __name__ == '__main__':
         distance_len = 5
         settings['blueprint_list'] = blueprint_list[:3]
         settings['weather_list'] = [40,60,80,100,150,180,200,220,250,280]
+        settings['spawnpoint_list'] = world.get_map().get_spawn_points()[:3]
         settings['theta_list'] = [i/theta_len * (math.pi / 2) for i in range(1, theta_len)] # without theta = 0, i.e., no overhead view
         settings['phi_list'] = [i/phi_len * (2 * math.pi) for i in range(phi_len)]
-        settings['spawnpoint_list'] = world.get_map().get_spawn_points()[:3]
-        settings['radius_list'] = [i/distance_len * 10 + 5 for i in range(distance_len)]
+        settings['radius_list'] = [i/distance_len * (distance_range[1] - distance_range[0]) + distance_range[0] for i in range(distance_len)]
 
     settings = settings_complete(blueprint_list, settings)
 
