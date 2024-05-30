dc1679df322c16a7e8f95365e339c08e52596cbb
Jianhong Pan <JianhongPan@outlook.com>
2024-05-31 01:51:31 +0800

    only tick when get image

--- export_datasets_with_coco_label.py
+++ export_datasets_with_coco_label.py
@@ -173,6 +173,10 @@ if __name__ == '__main__':
         settings['theta_list'] = [i/theta_len * (math.pi / 2) for i in range(1, theta_len)] # without theta = 0, i.e., no overhead view
         settings['phi_list'] = [i/phi_len * (2 * math.pi) for i in range(phi_len)]
 
+    if args.benchmark == 'weather':
+        dataset_name='weather'
+        settings['weather_list'] = [i/default_dataset_len * 1000 for i in range(1, default_dataset_len)]
+
     if args.benchmark == 'rotation-theta':
         dataset_name='rotation-theta'
         settings['theta_list'] = [i/default_dataset_len * (math.pi / 2) for i in range(default_dataset_len)]
@@ -191,10 +195,6 @@ if __name__ == '__main__':
         dataset_name='distance'
         settings['radius_list'] = [i/default_dataset_len * 10 + 4 for i in range(default_dataset_len)]
 
-    if args.benchmark == 'weather':
-        dataset_name='weather'
-        settings['weather_list'] = [i/default_dataset_len * 1000 for i in range(1, default_dataset_len)]
-    
     if args.benchmark == 'entire':
         dataset_name='entire'
         theta_len = 3
@@ -203,10 +203,10 @@ if __name__ == '__main__':
         weather_len = 10
         settings['blueprint_list'] = blueprint_list[:3]
         settings['spawnpoint_list'] = world.get_map().get_spawn_points()[:3]
+        settings['weather_list'] = [40,60,80,100,150,180,200,220,250,280]
         settings['theta_list'] = [i/theta_len * (math.pi / 2) for i in range(1, theta_len)] # without theta = 0, i.e., no overhead view
         settings['phi_list'] = [i/phi_len * (2 * math.pi) for i in range(phi_len)]
         settings['radius_list'] = [i/distance_len * 10 + 7 for i in range(distance_len)]
-        settings['weather_list'] = [40,60,80,100,150,180,200,220,250,280]
 
     settings = settings_complete(blueprint_list, settings)
 
